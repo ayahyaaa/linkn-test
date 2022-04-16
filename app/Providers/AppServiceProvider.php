9e21f33d1a6d04713e4eee74cccb1dd51dffba5b
ayahyaaa <k.naufalyahya@gmail.com>
2022-04-16 10:42:49 +0700

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->isSecure()) {
-            \URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 
